64f6cd0b607f4fbbf122cdcd2339330eb41d09e4
unknown <artbyphysics@outlook.com>
2025-05-08 21:14:47 +0200

    Added storage API

--- src/utils/uploadToIPFS.ts
+++ src/utils/uploadToIPFS.ts
@@ -1,9 +1,8 @@
 // src/utils/uploadToIPFS.ts
 import { NFTStorage, File } from 'nft.storage'
 
-const NFT_STORAGE_TOKEN = 'a54a0191.e2a1ad8edc024240bec46dab018d2431'
 
-const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
+const client = new NFTStorage({ token: import.meta.env.VITE_NFT_STORAGE_TOKEN });
 
 export async function uploadImageAndMetadata(blob: Blob) {
   const metadata = await client.store({
