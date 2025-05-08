974eeee1bda7960605093e1aa435d8584e6d7dee
unknown <artbyphysics@outlook.com>
2025-05-08 22:14:28 +0200

    Debug

--- src/utils/uploadToIPFS.ts
+++ src/utils/uploadToIPFS.ts
@@ -1,6 +1,6 @@
 // src/utils/uploadToIPFS.ts
 import { NFTStorage, File } from 'nft.storage'
-
+console.log("Token preview:", import.meta.env.VITE_NFT_STORAGE_TOKEN?.slice(0, 6));
 const client = new NFTStorage({ token: import.meta.env.VITE_NFT_STORAGE_TOKEN });
 
 export async function uploadImageAndMetadata(blob: Blob) {
