b079f826f351c1564384cd10a18bf8ee80b0b25a
unknown <artbyphysics@outlook.com>
2025-05-10 10:48:38 +0200

    Refactoring main.tsx to use wagmi and thirdweb

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,32 @@ import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
 
 import App from "./App";
-import { config } from "./wagmi.ts";
+import { config } from "./wagmi";
 
 import "./index.css";
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
+
+// Thirdweb
+import { ThirdwebProvider, createThirdwebClient } from "thirdweb/react";
+import { defineChain } from "thirdweb/chains";
 
 window.Buffer = Buffer;
 
+// Initialize clients
 const queryClient = new QueryClient();
+const thirdwebClient = createThirdwebClient({
+  clientId: import.meta.env.VITE_THIRDWEB_CLIENT_ID,
+});
+const chain = defineChain(8453); // Base mainnet
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ThirdwebProvider client={thirdwebClient} activeChain={chain}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThirdwebProvider>
     </WagmiProvider>
   </React.StrictMode>
 );
