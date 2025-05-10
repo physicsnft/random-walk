988c3c872759bb62574eeab9d3bb9f5d1349eafd
unknown <artbyphysics@outlook.com>
2025-05-10 11:35:46 +0200

    Bug fixes for new thirdweb sdk

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./index.css";
 
 import { ThirdwebProvider } from "thirdweb/react";
-import { thirdwebConfig } from "./thirdweb";
 
 import { WagmiProvider } from "wagmi";
 import { config as wagmiConfig } from "./wagmi";
@@ -12,13 +11,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
+ 
+export const client = createThirdwebClient({
+  clientId: import.meta.env.VITE_THIRDWEB_CLIENT_ID,
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
-      <ThirdwebProvider config={thirdwebConfig}>
+      <ThirdwebProvider>
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
