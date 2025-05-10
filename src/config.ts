643b863c903bb38b6753c45ddffbe39cdf249a1d
unknown <artbyphysics@outlook.com>
2025-05-10 16:25:40 +0200

    Added contract address

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,11 @@ import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
 import { createThirdwebClient } from "thirdweb";
 
-import abi from "./abi"; // your ABI JSON or object
+//import abi from "./abi"; // your ABI JSON or object
 
 export const client = createThirdwebClient({
   clientId: import.meta.env.VITE_THIRDWEB_CLIENT_ID,
+  secretKey: import.meta.env.VITE_THIRDWEB_SECRET_KEY,
 });
 
 export const chain = defineChain(8453); // Base
@@ -14,6 +15,6 @@ export const chain = defineChain(8453); // Base
 export const contract = getContract({
   client,
   chain,
-  address: "0xYourContractAddress",
-  abi,
+  address: "0xDCf417A8416CA83d20652987f04c5341223dd9f1",
+  //abi,
 });
