647c637b74ec9bc2728852fcc58beb122adc7391
unknown <artbyphysics@outlook.com>
2025-05-11 11:57:25 +0200

    New Sepolia test contract

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ import { abi } from "./abi";
 
 export const contractConfig = {
   //address: "0xd5d82Cbf96Cec703e9c947aA18DAd1FfBe03943a",
-  address: "0xF49132a0cA3d0bc94d844e1f07C398b0dC5e5b44",
+  address: "0x9ab96bf53026f28d452e0b00c5eebd3e41c684f9",
   abi,
 };
