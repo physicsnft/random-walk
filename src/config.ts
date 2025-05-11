bde7134fcca55de61ecf98fcd3edca20f976934d
unknown <artbyphysics@outlook.com>
2025-05-11 11:26:33 +0200

    Test contract

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { abi } from "./abi"; 
 
 export const contractConfig = {
-  address: "0xd5d82Cbf96Cec703e9c947aA18DAd1FfBe03943a",
+  //address: "0xd5d82Cbf96Cec703e9c947aA18DAd1FfBe03943a",
+  address: "0xF49132a0cA3d0bc94d844e1f07C398b0dC5e5b44",
   abi,
 };
