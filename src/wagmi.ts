9c30caeaf53722e44ee5229d3329ba9189ba1152
unknown <artbyphysics@outlook.com>
2025-05-11 17:38:15 +0200

    Added MM connector

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,14 @@
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+//import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { http, createConfig } from "wagmi";
 //import { base, mainnet } from "wagmi/chains";
 import { baseSepolia } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
 
 export const config = createConfig({
   //chains: [base, mainnet],
   chains: [baseSepolia],
-  connectors: [farcasterFrame()],
+  //connectors: [farcasterFrame()],
+  connectors: [injected()],
   transports: {
     [baseSepolia.id]: http(),
     //[base.id]: http(),
